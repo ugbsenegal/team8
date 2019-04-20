3c38bd7d481a4a9713699e96e18796487ed39169
Ndeye Khady Aidara <fatimaaidara953@gmail.com>
2019-04-20 02:03:46 +0000

    new

--- app/src/main/java/com/example/projetandroid/MainActivity.java
+++ app/src/main/java/com/example/projetandroid/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
